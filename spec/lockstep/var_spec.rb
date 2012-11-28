5956886906fa40e0f5c5b0b9c5cf20d8323fbd00
Scott Fleckenstein <nullstyle@gmail.com>
2012-11-28 09:33:57 -0800

    Extracks @tick_size from magic numbers in var_spec

--- spec/lockstep/var_spec.rb
+++ spec/lockstep/var_spec.rb
@@ -5,10 +5,11 @@ module Lockstep
     before(:each) do 
       @storage = Storage::Memory.new
       @time_base = Time.now  
+      @tick_size = 10
     end
     
     subject do
-      Var.new(@storage, "foo", 10)
+      Var.new(@storage, "foo", @tick_size)
     end
   
     describe "#get" do
@@ -17,7 +18,7 @@ module Lockstep
       end
       
       it "should return nil if no tuples stored could be considered active" do
-        @storage.write("foo", @time_base + 1 , 10, 10)
+        @storage.write("foo", @time_base + 1 , 10, @tick_size)
         subject.get(@time_base).should be_nil
       end
       
@@ -27,20 +28,20 @@ module Lockstep
       end
       
       it "should return the newest value (based upon active_at) that is active" do
-        @storage.write("foo", @time_base - 10, 10, 10)
-        @storage.write("foo", @time_base - 5, 20, 10)
+        @storage.write("foo", @time_base - 10, 10, @tick_size)
+        @storage.write("foo", @time_base - 5, 20, @tick_size)
         subject.get(@time_base).should eq(20)
       end
       
       it "should return a value that is made active on the second it becomes available" do
-        @storage.write("foo", @time_base, 10, 10)
+        @storage.write("foo", @time_base, 10, @tick_size)
         subject.get(@time_base).should eq(10)
       end
       
       
       it "should return the oldest active value, even if a future value is scheduled" do
-        @storage.write("foo", @time_base, 10, 10)
-        @storage.write("foo", @time_base + 1, 20, 10)
+        @storage.write("foo", @time_base, 10, @tick_size)
+        @storage.write("foo", @time_base + 1, 20, @tick_size)
         subject.get(@time_base).should eq(10)
       end
     end
@@ -51,20 +52,20 @@ module Lockstep
     end
     
     describe "#next_check_at" do
-      before(:each){ @storage.write("foo", @time_base, 10, 10) }
+      before(:each){ @storage.write("foo", @time_base, 10, @tick_size) }
       
       it "should return a Time value" do
         subject.next_check_at.should be_instance_of(Time)
       end
             
       it "should return the current_time + 1 tick if the current_time is on a tick boundary" do
-        subject.next_check_at(@time_base).should == @time_base + 10
-        subject.next_check_at(@time_base + 10).should == @time_base + 20
+        subject.next_check_at(@time_base).should == @time_base + @tick_size
+        subject.next_check_at(@time_base + 10).should == @time_base + (@tick_size * 2)
       end
       
       it "should return the current_time + (1 tick - tick size) if the current_time is not on a tick boundary" do
-        subject.next_check_at(@time_base + 3).should == @time_base + 10
-        subject.next_check_at(@time_base + 13).should == @time_base + 20
+        subject.next_check_at(@time_base + 3).should == @time_base + @tick_size
+        subject.next_check_at(@time_base + 13).should == @time_base + (@tick_size * 2)
       end
       
     end
@@ -85,7 +86,7 @@ module Lockstep
       
       it "should overwrite the old tuples with the new values" do
         orig = subject.tuples
-        @storage.write("foo", @time_base, 10, 10)
+        @storage.write("foo", @time_base, 10, @tick_size)
         subject.refresh(@time_base)
         subject.tuples.should_not == orig
       end
