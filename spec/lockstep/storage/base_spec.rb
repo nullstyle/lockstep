89b5522db72810da00527aa76bf32fde5153c2e0
Scott Fleckenstein <nullstyle@gmail.com>
2013-07-30 20:02:37 -0700

    Rewrites storage/base_spec.rb as rspec-given

--- spec/lockstep/storage/base_spec.rb
+++ spec/lockstep/storage/base_spec.rb
@@ -2,20 +2,17 @@ require 'spec_helper'
 
 describe Lockstep::Storage::Base do
   describe "#read" do
-    it "should raise a runtime error" do
-       expect { subject.read("my_key") }.to raise_error
-    end
+    When(:result) { subject.read("my_key") }
+    Then { result.should have_failed }
   end  
   
   describe "#remove" do
-    it "should raise a runtime error" do
-       expect { subject.remove("my_key", Time.now) }.to raise_error
-    end
+    When(:result) { subject.remove("my_key") }
+    Then { result.should have_failed }
   end
   
   describe "#write" do
-    it "should raise a runtime error" do
-       expect { subject.write("my_key", Time.now, 3, 10) }.to raise_error
-    end
+    When(:result) { subject.write("my_key") }
+    Then { result.should have_failed }
   end
 end
